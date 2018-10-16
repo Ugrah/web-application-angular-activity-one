3bc1151cae551ebcd229b79f90b8c22d65bf09e9
Ugrah <lobognonulrichgrah@outlook.fr>
2018-10-17 00:42:00 +0100

    Mise à jour des objets Posts du app.component.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,20 @@ export class AppComponent {
 
   posts = [
     {
-      title: 'Mon premier post',
+      title: 'Mon premier Post',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus diam, molestie dapibus nulla quis, facilisis luctus felis. Aenean sit amet risus ipsum. Aliquam ligula sapien, dictum ac bibendum vehicula, tincidunt quis augue. Etiam suscipit diam nisl. Vestibulum at eros eu urna vestibulum placerat in a ex. Proin vitae odio fermentum, pharetra augue a, malesuada eros. Nam vestibulum malesuada pharetra.',
       create_at: new Date('2016-12-17T03:24:00'),
       loveIts: 0
     },
     {
-      title: 'La fouine Lache enfin sa Double mixtape',
-      content: 'Integer blandit cursus felis nec ultrices. Curabitur est augue, elementum vel nisi placerat, sodales efficitur justo. Morbi mi nisi, euismod eget gravida vel, condimentum quis leo. Phasellus nec convallis tortor. Etiam feugiat metus eu magna sagittis venenatis. Aliquam rutrum erat non condimentum suscipit. Nullam semper vitae dolor euismod suscipit. Morbi vel nibh nisl. Fusce nec nulla ac lorem tempor venenatis sit amet id felis. Cras blandit, tellus ac rhoncus venenatis, arcu nibh imperdiet urna, quis aliquam elit lectus non metus. Morbi ultricies nibh vel tortor dapibus, id laoreet ipsum rutrum. Proin elementum, libero vitae tristique accumsan, ligula dui tincidunt enim, vel congue libero risus sit amet orci.',
+      title: 'Un deuxième Post',
+      content: 'Morbi porttitor porta suscipit. Nunc sit amet dictum purus. Ut in arcu pellentesque, tincidunt massa nec, egestas neque. Vestibulum ultricies euismod turpis sed tempor. Nunc a purus diam. Aliquam scelerisque pretium metus vitae fermentum. Mauris suscipit, sapien et aliquam blandit, velit magna auctor nunc, sit amet euismod augue lectus at augue. Suspendisse dignissim fermentum erat non ultrices. Aliquam ligula diam, placerat et faucibus sed, efficitur vel sapien. Sed et aliquet nisi.',
       create_at: new Date(),
       loveIts: 0
     },
     {
-      title: 'Booba défonce l\'U Arena',
-      content: 'Aenean lobortis lorem ultrices porta vehicula. Suspendisse a sollicitudin augue. Phasellus a egestas leo. Pellentesque hendrerit tristique ligula in maximus. Sed tincidunt scelerisque sollicitudin. Sed dolor velit, bibendum eget nisi feugiat, dictum sagittis nisl. Sed tincidunt ullamcorper magna, nec ullamcorper quam feugiat a. Nulla mollis sapien at nunc feugiat, sit amet eleifend purus congue. Suspendisse quis velit sed justo efficitur laoreet ut vitae ipsum.',
+      title: 'Ceci est un autre Post',
+      content: 'Ut ac dapibus arcu, eu consectetur nunc. Sed dictum gravida metus, eu dapibus augue dictum eu. Nam quis ipsum rhoncus, iaculis risus aliquam, ultricies elit. Vivamus metus odio, placerat eu sem quis, feugiat feugiat odio. Morbi eros tellus, gravida vitae pharetra sed, finibus ac nibh. Curabitur non nisl feugiat, dapibus sapien et, viverra ante. Aliquam dictum orci sit amet nibh dapibus, porta euismod sapien ornare. Quisque tristique orci dictum eros euismod, ut placerat risus lacinia. Sed quis pellentesque odio. Praesent ante elit, pharetra sed volutpat non, auctor vitae ligula. Aenean neque velit, finibus sed dapibus at, ornare vel magna. Cras at urna bibendum, facilisis turpis eu, auctor mauris. Vivamus nisi orci, iaculis eu turpis at, bibendum maximus eros.',
       create_at: new Date(),
       loveIts: 0
     }
